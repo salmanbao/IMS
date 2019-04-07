cd0e65dad8c4be351a77676a7889ca480661ec56
salmanbao <salmancodez@gmail.com>
2019-04-07 11:56:11 +0500

    update components directory structure

--- src/app/lazyloading/blockchain/blockchain.module.ts
+++ src/app/lazyloading/blockchain/blockchain.module.ts
@@ -1,28 +1,28 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { BlockchainhomeComponent } from './blockchainhome/blockchainhome.component';
-import { ManagechannelsComponent } from './managechannels/managechannels.component';
 import { AddccComponent } from './addcc/addcc.component';
 import { ManagepeersComponent } from './managepeers/managepeers.component';
 import { BlockchainRoutingModule } from 'app/routings/blockchain.routing';
 import { MaterialModule } from 'app/modules/material.module';
 import { AddMemberComponent } from './add-member/add-member.component';
-import { ManageMembersComponent } from './manage-members/manage-members.component';
-import { ManageMembersCertificatesComponent } from './manage-members-certificates/manage-members-certificates.component';
-import { AddMembersCertificatesComponent  } from './add-members-certificates/add-members-certificates.component';
-import { AddChannelComponent } from './add-channel/add-channel.component';
 import { InitiateCCComponent } from './initiate-cc/initiate-cc.component';
 import { AddPeerComponent } from './add-peer/add-peer.component';
-import { ManageCertificatesAuthorityComponent } from './manage-certificates-authority/manage-certificates-authority.component';
 import { AddUserComponent } from './add-user/add-user.component';
-import { ListOrgMembersComponent } from './list-org-members/list-org-members.component';
-import { ManageNotificationsComponent } from './manage-notifications/manage-notifications.component';
-import { AllNotificationsComponent } from './all-notifications/all-notifications.component';
-import { PendingNotificationsComponent } from './pending-notifications/pending-notifications.component';
-import { CompletedNotificationsComponent } from './completed-notifications/completed-notifications.component';
-import { AddMemberDialogComponent } from './list-org-members/add-member-dialog/add-member-dialog.component';
-import { FormsModule } from '@angular/forms';
-import { AddMemberCertificatesDialogComponent } from './add-members-certificates/add-member-certificates-dialog/add-member-certificates-dialog.component';
+import { ManagechannelsComponent } from './channel/managechannels/managechannels.component';
+import { ManageMembersComponent } from './organization/manage-members/manage-members.component';
+import { AddMembersCertificatesComponent } from './organization/add-members-certificates/add-members-certificates.component';
+import { AddChannelComponent } from './channel/add-channel/add-channel.component';
+import { ManageCertificatesAuthorityComponent } from './ca/manage-certificates-authority/manage-certificates-authority.component';
+import { ListOrgMembersComponent } from './organization/list-org-members/list-org-members.component';
+import { ManageNotificationsComponent } from './notification/manage-notifications/manage-notifications.component';
+import { AllNotificationsComponent } from './notification/all-notifications/all-notifications.component';
+import { PendingNotificationsComponent } from './notification/pending-notifications/pending-notifications.component';
+import { CompletedNotificationsComponent } from './notification/completed-notifications/completed-notifications.component';
+import { AddMemberDialogComponent } from './organization/list-org-members/add-member-dialog/add-member-dialog.component';
+import { AddMemberCertificatesDialogComponent } from './organization/add-members-certificates/add-member-certificates-dialog/add-member-certificates-dialog.component';
+
 
 
 @NgModule({
@@ -39,7 +39,7 @@ import { AddMemberCertificatesDialogComponent } from './add-members-certificates
     ManagepeersComponent,
     AddMemberComponent,
     ManageMembersComponent,
-    ManageMembersCertificatesComponent,
+   
     AddMembersCertificatesComponent,
     AddChannelComponent,
     InitiateCCComponent,
