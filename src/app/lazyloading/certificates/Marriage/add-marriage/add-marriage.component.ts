96a9eef459a226c3bdbcd2b7ebe8b79f1d4dd5ce
MUHAMMAD ABDULLAH QURESHI <15009065052>
2019-04-11 14:10:03 +0500

    marriage components

--- src/app/lazyloading/certificates/Marriage/add-marriage/add-marriage.component.ts
+++ src/app/lazyloading/certificates/Marriage/add-marriage/add-marriage.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { City } from 'app/modules/cities.module';
+import { BasicInfo } from 'app/modules/BasicInfo.module';
+import { PersonalInfoService } from 'app/services/personal-info.service';
 
 @Component({
   selector: 'app-add-marriage',
@@ -7,9 +11,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddMarriageComponent implements OnInit {
 
-  constructor() { }
+  value: string = '';
+  minDate = new Date(1950, 0, 1);
+  addBirthForm: FormGroup;
+  maxDate = new Date();
+  genders: Array<string> = ['Male', 'Female'];
+  regionInfo: City = new City();
+  cities: any = this.regionInfo.cities;
+  _province: string = '';
+  _country: string = '';
+  religions: any = this.regionInfo.religions;
+  professions: Array<string> = new BasicInfo().professions;
+  _martialStatus: Array<string> = new BasicInfo().martialStatus;
+  constructor(
+    private _formBuilder: FormBuilder,
+    private userService: PersonalInfoService
+  ) { }
 
   ngOnInit() {
+    this.addBirthForm = this._formBuilder.group({
+
+      did: ['', null],
+      fname: ['', Validators.required],
+      lname: ['', Validators.required],
+      fatherDID: ['', Validators.required],
+      motherDID: ['', Validators.required],
+      familyNumber: ['', Validators.required],
+      husbandAddress: ['', Validators.required],
+      husbandDOB: ['', Validators.required],
+      wifeDOB: ['', Validators.required],
+      wifeAddress: ['', Validators.required],
+      witnessOne: ['', Validators.required],
+      witnessTwo: ['', Validators.required],
+      witnessThird: ['', Validators.required],
+      MarriageDate: ['', Validators.required],
+    })
   }
+
+  //### Create citizen ######
+  registerUser() {
+    console.log(this.addBirthForm);
+    console.log(JSON.stringify(this.addBirthForm.value))
+
+    this.userService.create(this.addBirthForm.value)
+      .subscribe(
+        data => {
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  getData() {
+    //send request to server for generating DID and return
+    console.log("generate Family Number");
+    this.addBirthForm.controls['familyNumber'].setValue("LHR1234");
+  }
+
+ 
 
 }
--- src/app/routings/personalinfo.routing.ts
+++ src/app/routings/personalinfo.routing.ts
@@ -4,15 +4,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { CertificatehomeComponent } from 'app/lazyloading/certificates/certificatehome/certificatehome.component';
 import { NationalIdHomeComponent } from 'app/lazyloading/certificates/National/national-id-home/national-id-home.component';
 import { BirthHomeComponent } from 'app/lazyloading/certificates/Birth/birth-home/birth-home.component';
-import { ListMarriageComponent } from 'app/lazyloading/certificates/Marriage/list-marriage/list-marriage.component';
 import { FamilyHomeComponent } from 'app/lazyloading/certificates/Family/family-home/family-home.component';
+import { HomeMarriageComponent } from 'app/lazyloading/certificates/Marriage/home-marriage/home-marriage.component';
 
 
 const routes: Routes = [
   { path: '', component: CertificatehomeComponent },
   { path: 'birth', component: BirthHomeComponent },
   { path: 'nationalidlist', component: NationalIdHomeComponent },
-  { path: 'marriage', component: ListMarriageComponent },
+  { path: 'marriage', component: HomeMarriageComponent },
   { path: 'family', component: FamilyHomeComponent }
 ];
 
