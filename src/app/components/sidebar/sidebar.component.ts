492468f0e6d84241b836d0514dcf93e8cf57aca8
MUHAMMAD ABDULLAH QURESHI <15009065052>
2019-04-12 16:04:43 +0500

    compelete family tree

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -10,17 +10,17 @@ declare interface RouteInfo {
 export const ROUTES: RouteInfo[] = [
     { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
     { path: '/certificates', title: 'Certificates',  icon: 'card_giftcard', class: '' },
-    { path: '/users', title: 'Users',  icon: 'supervisor_account', class: '' },
+    //{ path: '/users', title: 'Users',  icon: 'supervisor_account', class: '' },
     { path: '/blockchain', title: 'Blockchain',  icon: 'view_week', class: '' },
     { path: '/reports', title: 'Reports',  icon: 'assignment', class: '' },
 
 
-    { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
-    { path: '/table-list', title: 'Table List',  icon:'content_paste', class: '' },
-    { path: '/typography', title: 'Typography',  icon:'library_books', class: '' },
-    { path: '/icons', title: 'Icons',  icon:'bubble_chart', class: '' },
-    { path: '/maps', title: 'Maps',  icon:'location_on', class: '' },
-    { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' }
+   // { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
+   // { path: '/table-list', title: 'Table List',  icon:'content_paste', class: '' },
+   // { path: '/typography', title: 'Typography',  icon:'library_books', class: '' },
+    //{ path: '/icons', title: 'Icons',  icon:'bubble_chart', class: '' },
+    //{ path: '/maps', title: 'Maps',  icon:'location_on', class: '' },
+    //{ path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' }
 ];
 
 @Component({
