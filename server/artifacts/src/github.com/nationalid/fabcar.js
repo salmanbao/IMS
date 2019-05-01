0ea326f96e4b03036cb75a3f17c0c68cc8eb1481
salmanbao <salmancodez@gmail.com>
2019-04-30 22:42:13 -0700

    updating chaincode

--- server/artifacts/src/github.com/nationalid/fabcar.js
+++ server/artifacts/src/github.com/nationalid/fabcar.js
@@ -137,94 +137,138 @@ let Chaincode = class {
     }
 
     //----------- Insert NationalID------------
-  async InsertNationalId(stub) {
-    console.log('=================Insertion of national-id');
-    let ret = stub.getFunctionAndParameters();
-    console.log(ret);
-    let args = ret.params;
-    if (args.length != 11) {
-      return shim.error('Incorrect number of arguments. Expecting 11');
-    }
-    var info = {
-        did: args[0],
-        picture: args[1],
-        name: {
-            fname:args[2].fname,
-            lname:args[2].lname
-        },
-        father: args[3],
-        mother: args[4],
-        dob: args[5],
-        familyNumber: args[6],
-        religion: args[7],
-        profession: args[8],
-        education: args[9],
-        address: {
-            street:args[10].street,
-            province:args[10].province,
-            division:args[10].division,
-            district:args[10].district,
-            tehsile:args[10].tehsile
-        },
-        relations:{
-                mother:args[11].mother,
-                father:args[11].father,
-                spouse:args[11].spouse,
-                sons:args[11].sons,
-                daughters:args[11].daughters
+    async addNationalId(stub) {
+        console.log('=================Insertion of national-id');
+        let ret = stub.getFunctionAndParameters();
+        console.log(ret);
+        let args = ret.params;
+        if (args.length != 11) {
+            return shim.error('Incorrect number of arguments. Expecting 11');
         }
-      };
-    if (this.isString(
-      [
-        info.did, info.picture, info.father, info.mother, info.dob,
-        info.religion, info.profession, info.education,
-        info.name.fname, info.name.lname, info.address.street,
-        info.address.province, info.address.division,
-        info.address.district, info.address.tehsile,
-        info.relations.mother, info.relations.father
-      ])) {
-      return shim.error('Expecting string values for some parameters');
+        var info = {
+            did: args[0],
+            picture: args[1],
+            name: {
+                fname: args[2].fname,
+                lname: args[2].lname
+            },
+            father: args[3],
+            mother: args[4],
+            dob: args[5],
+            familyNumber: args[6],
+            religion: args[7],
+            profession: args[8],
+            education: args[9],
+            address: {
+                street: args[10].street,
+                province: args[10].province,
+                division: args[10].division,
+                district: args[10].district,
+                tehsile: args[10].tehsile
+            },
+            relations: {
+                mother: args[11].mother,
+                father: args[11].father,
+                spouse: args[11].spouse,
+                sons: args[11].sons,
+                daughters: args[11].daughters
+            }
+        };
+        if (this.isString(
+                [
+                    info.did, info.picture, info.father, info.mother, info.dob,
+                    info.religion, info.profession, info.education,
+                    info.name.fname, info.name.lname, info.address.street,
+                    info.address.province, info.address.division,
+                    info.address.district, info.address.tehsile,
+                    info.relations.mother, info.relations.father
+                ])) {
+            return shim.error('Expecting string values for some parameters');
+        }
+
+        try {
+            await stub.putState(did, Buffer.from(info));
+            console.log('================ Inserted Successfully=========');
+            return shim.success();
+        } catch (error) {
+            return shim.error(err);
+        }
     }
 
-    try {
-      await stub.putState(did, Buffer.from(info));
-      console.log('================ Inserted Successfully=========');
-      return shim.success();
-    } catch (error) {
-      return shim.error(err);
-    }
-  }
+    //----------- Delete NationalID------------
+    async DeleteNationalId(stub) {
+        //---------------- Deleting NationID-----------
+        if (args.length != 1) {
+            throw new Error('Incorrect number of arguments. Expecting 1');
+        }
 
-  //----------- Delete NationalID------------
-  async InsertNationalId(stub) {
-    //---------------- Deleting NationID-----------
-    if (args.length != 1) {
-      throw new Error('Incorrect number of arguments. Expecting 1');
+        let did = args[0];
+
+        // Delete the key from the state in ledger
+        await stub.deleteState(did);
     }
 
-    let did = args[0];
+    async addBirthCertificate(stub) {
+        console.log('=================Insertion of Birth Certificate');
+        let ret = stub.getFunctionAndParameters();
+        console.log(ret);
+        let args = ret.params;
+        if (args.length != 11) {
+            return shim.error('Incorrect number of arguments. Expecting 11');
+        }
+        var info = {
+            did: args[0],
+            name: {
+                fname: args[1].fname,
+                lname: args[1].lname
+            },
+            father: args[2],
+            mother: args[3],
+            dob: args[4],
+            familyNumber: args[5],
+            religion: args[6],
+            gender: args[7],
+            address: [8],
+            city: [9],
+            country: [10]
+        };
+        if (this.isString(
+                [
+                    info.did, info.picture, info.father, info.mother, info.dob,
+                    info.religion, info.profession, info.education,
+                    info.name.fname, info.name.lname, info.address.street,
+                    info.address.province, info.address.division,
+                    info.address.district, info.address.tehsile,
+                    info.relations.mother, info.relations.father
+                ])) {
+            return shim.error('Expecting string values for some parameters');
+        }
 
-    // Delete the key from the state in ledger
-    await stub.deleteState(did);
-  }
+        try {
+            await stub.putState(did, Buffer.from(info));
+            console.log('================ Inserted Successfully=========');
+            return shim.success();
+        } catch (error) {
+            return shim.error(err);
+        }
+    }
 
-
-   isString(str) {
-    str.forEach(element => {
-      if (typeof element != 'string') {
-        return false;
-      }
-    });
-    return true;
-  }
-   isNumber(num) {
-    num.forEach(element => {
-      if (typeof element != 'number') {
-        return false;
-      }
-    });
-    return true;
-  }
+    isString(str) {
+        str.forEach(element => {
+            if (typeof element != 'string') {
+                return false;
+            }
+        });
+        return true;
+    }
+    isNumber(num) {
+        num.forEach(element => {
+            if (typeof element != 'number') {
+                return false;
+            }
+        });
+        return true;
+    }
 
 };
 
